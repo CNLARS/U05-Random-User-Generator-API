f1ce19d02a69d814fb47252518edd58d153ecaa1
CNLARS <53753876+CNLARS@users.noreply.github.com>
2020-04-23 18:32:40 -0700

    Employees + Search
    Each card displays different employee data, and the search bar filters results based on any information in their contact info.
    
    On deck, Modal Window and Refactoring to Semi-Final Quality.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,3 @@
-/* 
-jQuery Snippet from "https://randomuser.me/"
-
-$.ajax({
-    url: 'https://randomuser.me/api/',
-    dataType: 'json',
-    success: function(data) {
-      console.log(data);
-    }
-  });
-
-  */
-
 
 //Global Variables
 const form = document.createElement("form");
@@ -18,9 +5,9 @@ const gallery = document.getElementById("gallery");
 const card = document.getElementsByClassName("card");
 const searchContainer = document.querySelector(".search-container");
 
-//Appending a Search Bar to `search-container` div:
+//++EE, Appending a Search Bar to `search-container` div:
     function searchBar(){
-
+        //Where in the world is Carmen Sandiego?
     const htmlMarkup = `
         <form action="#" method="get">
             <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -28,14 +15,34 @@ const searchContainer = document.querySelector(".search-container");
         </form>
     `;
         searchContainer.innerHTML = htmlMarkup;
+        
+        //Employees are searchable by any information in their contact card
+            document.querySelector(".search-container input").addEventListener("keyup", (e) => {
+                //console.log(e.target.value);
+                const searchMatch = [];
+
+                for(let i = 0; i < card.length; i++){
+                    const contactInfo = document.querySelectorAll(".card")[i].innerText;
+                        card[i].style.display = "none";
+
+                    if( contactInfo.includes(e.target.value) ){
+                        //Search value includes contact push to new array to display
+                        searchMatch.push(contactInfo);
+                        card[i].style.display = "inherit";
+                        //console.log();
+                    } else {
+                        console.log("Who?");
+                        //Refactor to add message to user.
+                    }
+
+
+                }
+                
+
+            });
 
 /*
-
-EE Code: Employees can be filtered by name.
-        -Create an empty array.
-        -if/else style.display "block/none"
-        -if input search val includes contact push to new array to display
-
+EE Code: Employees can be filtered by all .
 */
 
     }
@@ -45,23 +52,20 @@ searchBar();  //Testing123
 
 //Appending Gallery to `gallery` div:
 function galleryMode(){
-    //Loops to randomly select 12 "Awesome Inc." employees
+
+    //++EE: Updated CSS
+        document.body.style.backgroundColor = "gold";
+        document.querySelector("div").style.backgroundColor = "goldenrod";
+        document.querySelector("div").style.textShadow = "2px 3px 4px floralwhite";
+
+//Create 12 "Awesome Inc." employee cards with HTML markup
     for(let i = 0; i < 12; i++){
         const htmlMarkup = `
-        <div class="card">
-            <div class="card-img-container">
-                <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-            </div>
-            <div class="card-info-container">
-                <h3 id="name" class="card-name cap">first last</h3>
-                <p class="card-text">email</p>
-                <p class="card-text cap">city, state, country, postcode</p>
-            </div>
-        </div>
+            <div class="card"></div>
         `;
-
+    
         gallery.innerHTML += htmlMarkup;
-        
+
     }
     
 }
@@ -74,88 +78,50 @@ function fetchData(url){
     return fetch(url)
       .then(checkStatus)
       .then(res => res.json() )
-      .catch(error => console.log(`Looks like ${error} is causing a problem fetching the requested information from ${url} at this time. ${response.statusText}`) )
+      .then(data => {
+        console.log(data.results); //Reference to compare data
+        
+            for(let i = 0; i < card.length; i++){
+                console.log(i); //Testing123 
+                //Success Test: console.log(`${data.results[i].name.first} ${data.results[i].name.last}`);
+            
+            const fullName = `${data.results[i].name.first} ${data.results[i].name.last}`;
+            const email = `${data.results[i].email}`;
+            const city = `${data.results[i].location.city}`;
+            const state = `${data.results[i].location.state}`;
+            const country = `${data.results[i].location.country}`;
+            const postalCode = `${data.results[i].location.postcode}`;
+            const profilePicture = `${data.results[i].picture.large}`;
+
+       //Where in the World is Carmen Sandiego?
+                card[i].innerHTML = `
+                    <div class="card-img-container">
+                    <img class="card-img" src="${profilePicture}" alt="profile picture">
+                    </div>
+                    <div class="card-info-container">
+                    <h3 id="name" class="card-name cap">${fullName}</h3>
+                    <p class="card-text">${email}</p>
+                    <p class="card-text cap">${city}, ${state}<br> ${country}<br>${postalCode}</p>
+                    </div>`
+        
+             console.log(fullName);
+
+            }
+        
+        })
+      .catch(error => console.log(`${error.statusText} occurred: Looks like "${error}" 
+        is causing a problem fetching the requested information from ${url} at this time.`) )
   
   }
 
   Promise.all([
-    fetchData("https://randomuser.me/api/")
+    fetchData(`https://randomuser.me/api/?results=${card.length}`)
   ])
   
     .then(data => {
-        console.log(data); //Testing123 for reference
-        const fullName = `${data[0].results[0].name.first} ${data[0].results[0].name.last}`;
-        const email = `${data[0].results[0].email}`;
-        const city = `${data[0].results[0].location.city}`;
-        const state = `${data[0].results[0].location.state}`;
-        const country = `${data[0].results[0].location.country}`;
-        const postalCode = `${data[0].results[0].location.postcode}`;
-        const profilePicture = `${data[0].results[0].picture.large}`;
 
-        
-        findEmployee(fullName, email, city, state, country, postalCode, profilePicture);
-
+             
     })
-
-  
-function findEmployee(nameData, emailData, cityData, stateData, countryData, postalData, pictureData){
-    //Where in the World is Carmen Sandiego?
-    
-    // for(let i = 0; i < card.length; i++){
-            
-    //         //Functional, concise, yet identical data...
-
-    //         card[i].innerHTML = `
-    //         <div class="card-img-container">
-    //             <img class="card-img" src="${pictureData}" alt="profile picture">
-    //         </div>
-    //         <div class="card-info-container">
-    //             <h3 id="name" class="card-name cap">${nameData}</h3>
-    //             <p class="card-text">${emailData}</p>
-    //             <p class="card-text cap">${cityData}, ${stateData}, ${countryData}, ${postalData}</p>
-    //         </div>
-       
-    //     `;
-    // }
-    console.log(Array.from(card));
-    
-        //Refactored, loops same info
-            // for(let i = 0; i < card.length; i++){
-
-            // const employeeName = document.getElementsByClassName("card-name")[i];
-            // const employeeEmail = document.getElementsByClassName("card-name")[i].nextElementSibling;
-            // const employeePhoto = document.getElementsByClassName("card-img")[i];
-            // const employeeLocation = document.getElementsByClassName("card-text cap")[i];
-
-            // employeeName.innerHTML = nameData;
-            // employeeEmail.innerHTML = emailData;
-            // employeePhoto.src = pictureData;
-            // employeeLocation.innerHTML = `${cityData}, ${stateData}<br> ${countryData}<br>${postalData}`;
-
-
-            // }
-            
-//console.log(card);
-        };
-
-        //Original, loops same info
-        // const employeeName = document.getElementsByClassName("card-name")[i];
-        //     employeeName.innerHTML = nameData;
-
-        // const employeeEmail = document.getElementsByClassName("card-name")[i].nextElementSibling;
-        //     employeeEmail.innerHTML = emailData;
-
-        // const employeePhoto = document.getElementsByClassName("card-img")[i];
-        //     employeePhoto.src = pictureData;
-
-        // const employeeLocation = document.getElementsByClassName("card-text cap")[i];
-        //     employeeLocation.innerHTML = `${cityData}, ${stateData}<br> ${countryData}<br>${postalData}`;
-
-    //}
-   
-
-    //}
-
 
 //Sidekick Function(s):
 function checkStatus(response){
@@ -167,4 +133,11 @@ function checkStatus(response){
   
   }
 
-  //To Be Continued:  Displaying different employee data to each card
+/*
+  To Be Continued:  
+  Then adding event listener to each card to view individual contact information.
+
+  ++EE: 
+  Create a functional Search Bar.
+  Navigational elements.
+ */
