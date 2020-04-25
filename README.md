fe1eb72a5015e9a3e2722dc35b3e390013b5984b
CNLARS <53753876+CNLARS@users.noreply.github.com>
2020-04-24 20:05:53 -0700

    Semi Finished
    +Mostly Meets Expectations!
    -There is not yet coded a way to close the modal window.
    
    EE.inProgress(Modal Window Navigation Elements)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # U05 Random User Generator API
  Employee Directory
+
+
+CSS Honeybee Theme:
+    -Two Tone Background Color Body/Search DIV
+    -Text Shadow adds contrast
+    -Inspirational Bee Emojis
+    -Modal Window Emoji Key
+
++Navigational Functionality:  
+    Switches between contact card details in Modal Window.
+
++Dynamic Search Bar.
--- scripts.js
+++ scripts.js
@@ -18,33 +18,33 @@ const searchContainer = document.querySelector(".search-container");
         
         //Employees are searchable by any information inside contact card:
             document.querySelector(".search-container input").addEventListener("keyup", (e) => {
-                //console.log(e.target.value);
                 const searchMatch = [];
 
                 for(let i = 0; i < card.length; i++){
-                    const contactInfo = document.querySelectorAll(".card")[i].innerText;
+
+                        const contactInfo = document.querySelectorAll(".card")[i].innerText;
                         card[i].style.display = "none";
 
                     if( contactInfo.includes(e.target.value) ){
                         //Search value includes contacts to push to new array for display:
                         searchMatch.push(contactInfo);
                         card[i].style.display = "inherit";
-                        //console.log(); Testing123?
-                    } else {
-                        //console.log("Who?"); //Testing123, understanding functionality  
-                    //Meta Error Message:
-                        const secretTelegraph = `<p>Where in the world is Carmen Sandiego?</p>`;
+                        }
+
+                        //Meta Error Message:
+                     if(searchMatch.length === 0){
+                        //console.log("Where's Waldo?"); Testing123
+                        const secretTelegraph = `
+                        <h2>Where in the world is Carmen Sandiego?<br>🌎 💃🏾 🔍<br></h2>
+                        `;
                         gallery.innerHTML = secretTelegraph;
-                    }
-
-                }
-
+                    } //Does not update when backspace/delete input!
+                    
+                }                 
+                    
             });
 
     }
-
-//Calls the function-RELOCATION UPON REFACTORING
-searchBar();  //Testing123
 
 
 //Appending Gallery of contact cards to `gallery` div:
@@ -62,12 +62,11 @@ function galleryMode(){
         `;
     
         gallery.innerHTML += htmlMarkup;
+        card[i].style.backgroundColor = "goldenrod";
 
     }
     
 }
-//Calls the function-RELOCATION UPON REFACTORING
-galleryMode();  //Testing123
 
 
 //Fetch Function: Retrieves Info from Requested url:
@@ -87,39 +86,86 @@ function fetchData(url){
             const state = `${data.results[i].location.state}`;
             const country = `${data.results[i].location.country}`;
             const postalCode = `${data.results[i].location.postcode}`;
+            const timeZone = `${data.results[i].location.timezone.offset}`;
             const profilePicture = `${data.results[i].picture.large}`;
+
+            const phoneNum = `${data.results[i].phone}`;
+            const streetName = `${data.results[i].location.street.name}`;
+            const addressNum = `${data.results[i].location.street.number}`;
+            
+            const birthday =  `${data.results[i].dob.date}`;
+            //console.log(new Date(birthday).toUTCString()); //Testing123 Reference Point
+            
+            const year = new Date(birthday).getFullYear();
+            const month = new Date(birthday).getMonth() + 1;
+            const date = new Date(birthday).getDate();
+            
+            
 
             //Where in the World is Carmen Sandiego?
                 card[i].innerHTML = `
                     <div class="card-img-container">
                     <img class="card-img" src="${profilePicture}" alt="profile picture">
-                    💡<br>
-                    🐝<br>
+                    <br>💡<br>🐝
                     </div>
                     <div class="card-info-container">
                     <h3 id="name" class="card-name cap">${fullName} 🌻</h3>
-                    <p class="card-text">${email}</p>
-                    <p class="card-text cap">${city}, ${state}<br> ${country}<br>${postalCode}</p>
+                    <p class="card-text"><strong>${email}</strong></p>
+                    <p class="card-text cap">${city}, ${country}<br><br> GMT: ${timeZone}</p>
                     </div>`
         
              console.log(fullName); //Testing123 Visibility
+            
+             //Sets event listener to all cards:
+             card[i].addEventListener("click", (e) => {
+                console.log("bzzz"); //Testing123
+
+                document.body.innerHTML = `
+                <div class="modal-container">
+                <div class="modal">
+                    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                    <div class="modal-info-container">
+                        <img class="modal-img" src="${profilePicture}" alt="profile picture">
+                        <h3 id="name" class="modal-name cap">${fullName}</h3>
+                        <p class="modal-text">📧 ${email}</p>
+                        <p class="modal-text cap">📍 ${city}</p>
+                        <hr>
+                        <p class="modal-text">☎️ ${phoneNum}</p>
+                        <p class="modal-text">📬 ${addressNum} ${streetName}<br>${city}, ${state}<br>${country}<br>${postalCode}</p>
+                        <p class="modal-text">🎁 Birthday: ${month}/${date}/${year}</p>
+                    </div>
+                
+                    <div class="modal-btn-container">
+                        <button type="button" id="modal-prev" class="modal-prev btn">Prev</button>
+                        <button type="button" id="modal-next" class="modal-next btn">Next</button>
+                    </div>
+                </div>
+                `
+                //console.log(document.body.innerHTML); //Testing123 Still needs ++EE Criteria
+
+                //add event listener and if conditional 
+                //with i + 1 for right and i - 1 for left and modal-close-btn!
+                document.getElementsByClassName("modal-container").addEventListener("click", (e) => {
+                    console.log("Where's Waldo?");
+                });
+
+            });
 
             }
-        
+   
         })
-      .catch(error => console.log(`Error occurred: Looks like "${error}" 
-        is causing a problem fetching the requested information from ${url} at this time.`) )
+        
+
+      .catch(error => 
+        console.log(`ERROR: "${error}" and is unable to provide the requested information from ${url} at this time.`) )
   
   }
 
-  Promise.all([
-    fetchData(`https://randomuser.me/api/?results=${card.length}`)
-  ])
-  
-    .then(data => {
-        //Study up on .then() after Promise.all()!
-             
-    })
+//Calling Functions:
+searchBar();
+    galleryMode();
+        fetchData(`https://randomuser.me/api/?results=${card.length}`);
+
 
 //Sidekick Function(s):
 function checkStatus(response){
@@ -131,11 +177,26 @@ function checkStatus(response){
   
   }
 
+/******
+    For Future Use:
+
+        Promise.all([
+
+        ])
+            .then(data => {
+            
+            })
+******/
+
+
+
 /*
   To Code List:  
-  Adding event listener to each card to view additional contact information on individual employee.
+  Switching contact card on individual employee when "click" event occurs on navigation.
+  X to close Modal Window.
 
   ++EE: 
-  Navigational elements for Modal Window
+  Navigational event listeners.
+  X to Close Window.
 
  */
